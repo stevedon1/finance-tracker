b0601238a3353599ce4c51fcd5d18b4e6e77f7d7
Steve Dongich <stevowny@gmail.com>
2025-02-28 19:16:44 +0300

    Install firebase and create body component

---
+++ components/Body.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Body = () => {
+  return (
+    <View>
+      <Text>Your Transactions</Text>
+      
+    </View>
+  )
+}
+
+export default Body
